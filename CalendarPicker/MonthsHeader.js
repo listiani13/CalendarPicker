dcd1a1e2557d5691c21c03018927d5c9b7198e31
listiani13 <listiliz13@gmail.com>
2020-11-11 12:28:00 +0700

    fix: text style proptypes in web

--- CalendarPicker/MonthsHeader.js
+++ CalendarPicker/MonthsHeader.js
@@ -1,35 +1,24 @@
-import React from 'react';
-import {
-  View,
-  Text,
-  Platform,
-} from 'react-native';
-import PropTypes from 'prop-types';
+import React from "react";
+import { View, Text, Platform } from "react-native";
+import PropTypes from "prop-types";
 
 export default function MonthsHeader(props) {
-  const {
-    styles,
-    textStyle,
-    headingLevel,
-    title,
-  } = props;
+	const { styles, textStyle, headingLevel, title } = props;
 
-  const accessibilityProps = { accessibilityRole: 'header' };
-  if (Platform.OS === 'web') {
-    accessibilityProps['aria-level'] = headingLevel;
-  }
+	const accessibilityProps = { accessibilityRole: "header" };
+	if (Platform.OS === "web") {
+		accessibilityProps["aria-level"] = headingLevel;
+	}
 
-  return (
-    <View style={styles.headerWrapper}>
-      <Text style={[styles.monthsHeaderText, textStyle]}>
-        { title }
-      </Text>
-    </View>
-  );
+	return (
+		<View style={styles.headerWrapper}>
+			<Text style={[styles.monthsHeaderText, textStyle]}>{title}</Text>
+		</View>
+	);
 }
 
 MonthsHeader.propTypes = {
-  styles: PropTypes.shape(),
-  textStyle: Text.propTypes.style,
-  title: PropTypes.string
+	styles: PropTypes.shape(),
+	textStyle: PropTypes.any,
+	title: PropTypes.string,
 };
